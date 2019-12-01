979659c385f1ee9c9131917a0ae2fdbff3d8e28b
Antonio Vanegas <hpsaturn@gmail.com>
2019-12-01 14:22:16 +0100

    initial commit. Basic flutter project structure

---
+++ android/app/src/main/kotlin/co/n21/org/self_beat/MainActivity.kt
@@ -0,0 +1,13 @@
+package co.n21.org.self_beat
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
